8cab1778087f5ba5c1d823ea60f0619af2fc5454
Kalebros <morgulero@gmail.com>
2015-11-29 18:21:30 +0100

    Paneles preeliminares

---
+++ Assets/AssetNegociacion/Scripts/NegotiationPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NegotiationPlayer : MonoBehaviour {
+
+
+    public GameObject panel;
+    private bool _isDialogOn = false;
+
+	// Use this for initialization
+	void Start () {
+        panel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DialogObject"))
+        {
+            initDialog();
+        }
+    }
+
+    void initDialog()
+    {
+        if (!_isDialogOn)
+        {
+            _isDialogOn = true;
+            print("Hola!");
+            panel.SetActive(true);
+        }
+    }
+
+}
